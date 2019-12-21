b9ff5d7f20ba8c57b1da77079189ee1278a71408
neilhanhan <1138418834@qq.com>
2019-12-21 15:29:45 +0800

    lwh

--- src/main/java/com/xmu/discount/DiscountApplication.java
+++ src/main/java/com/xmu/discount/DiscountApplication.java
@@ -3,16 +3,10 @@ package com.xmu.discount;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  *@author Liuwenhan
  */
-@EnableDiscoveryClient
-@EnableEurekaClient
-@EnableFeignClients
 @SpringBootApplication
 @MapperScan("com.xmu.discount.mapper")
 public class DiscountApplication
