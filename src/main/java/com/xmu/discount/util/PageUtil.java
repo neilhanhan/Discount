f18d55435a8dafacda3249ea4e36f25952dd95bb
BingyuanZhang <1176045997@qq.com>
2019-12-21 17:32:28 +0800

    张冰源commit，修改了discountController，修改了注释和返回状态码 写了一个pageUtil，用于分页

---
+++ src/main/java/com/xmu/discount/util/PageUtil.java
@@ -0,0 +1,52 @@
+package com.xmu.discount.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Zhang BingYuan
+ * @Date 2019/12/21 17:06
+ */
+public class PageUtil {
+    /**
+     * 判断page，limit参数是否正确不在这里判断
+     * @param page
+     * @param limit
+     * @param objects
+     * @return
+     */
+    public List<Object> pageStart(Integer page, Integer limit, List<Object> objects) {
+        /**
+         * 从数据库查找的对象list为空
+         */
+        if (objects.size()==0) {
+            return new ArrayList<Object>();
+        }
+
+        /**
+         * 返回null是参数错误
+         */
+        if (page>getMaxPageNum(objects,limit)) {
+            return null;
+        }else if (page<getMaxPageNum(objects,limit)) {
+            List<Object> objects1 = objects.subList((page - 1) * limit, page * limit);
+            return objects1;
+        }else if (page==getMaxPageNum(objects,limit)) {
+            List<Object> objects1 = objects.subList((page - 1) * limit, objects.size());
+            return objects1;
+        }
+        /**
+         * 理论上这个return不会执行
+         */
+        return null;
+    }
+
+    public int getMaxPageNum(List<Object> objects, Integer limit) {
+        if (objects.size()%limit!=0) {
+            return objects.size()/limit+1;
+        }
+        else {
+            return objects.size()/limit;
+        }
+    }
+}
