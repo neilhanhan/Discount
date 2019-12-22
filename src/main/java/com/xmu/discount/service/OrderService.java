503766169ddcc7d46a34df91bb302b9029d65aa4
neilhanhan <1138418834@qq.com>
2019-12-22 21:40:17 +0800

    2019年12月22日21:40:10

--- src/main/java/com/xmu/discount/service/OrderService.java
+++ src/main/java/com/xmu/discount/service/OrderService.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
  * @author Liuwenhan
  */
 @Component
-@FeignClient(value = "orderService")
+@FeignClient(name = "orderService")
 public interface OrderService {
 
     /**
